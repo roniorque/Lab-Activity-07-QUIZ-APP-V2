53dce61e0eb45e052024d986b6358f788ace723e
Ronio Jerico Roque <roniojericho144@gmail.com>
2023-04-29 14:59:30 +0800

    update

--- quiz.php
+++ quiz.php
@@ -1,26 +1,19 @@
 <?php
 
-
 require "vendor/autoload.php";
-
 
 session_start();
 
-
 // 3.
 
-
 use App\QuestionManager;
-
 
 $number = null;
 $question = null;
 
-
 try {
     $manager = new QuestionManager;
     $manager->initialize();
-
 
     if (isset($_SESSION['is_quiz_started'])) {
         $number = $_SESSION['current_question_number'];
@@ -31,12 +24,17 @@ try {
         $number = 1;
     }
 
-
-    if (isset($_POST['answer'])) {
-        $_SESSION['answers'][$number] = $_POST['answer'];
-        $number++;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Loop through all the questions and save answers in session
+        for($i = 1; $i <= 10; $i++) {
+            if (isset($_POST['answer'.$i])) {
+                $_SESSION['answers'][$i] = $_POST['answer'.$i];
+            }
+        }
+        // Redirect to result.php after submitting answers
+        header("Location: result.php");
+        exit;
     }
-
 
     // Has user answered all items
     if ($number > $manager->getQuestionSize()) {
@@ -44,10 +42,8 @@ try {
         exit;
     }
 
-
     // Marker for question number
     $_SESSION['current_question_number'] = $number;
-
 
     $question = $manager->retrieveQuestion($number);
 } catch (Exception $e) {
@@ -56,7 +52,6 @@ try {
     exit;
 }
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -67,50 +62,38 @@ try {
 </head>
 <body>
 
-
 <h1>Analogy Questions</h1>
 
-
 <h3>Instructions</h3>
-
 
 <p style="color: gray">
     There is a certain relationship between two given words on one side of : : and one word is given on another side of : : while another word is to be found from the given alternatives, having the same relation with this word as the words of the given pair bear. Choose the correct alternative.
 </p>
 
-
-<?php
-for ($number = 1; $number <= 10; $number++): ?>
-    <?php $question = $question = $manager->retrieveQuestion($number); ?>
+<form method="POST" action="quiz.php">
+    <?php for($number=1;$number<=10;$number++): ?>
+    
+        <?php $question = $manager->retrieveQuestion($number); ?>
         <h1>Question #<?php echo $question->getNumber(); ?></h1>
         <h2 style="color: blue"><?php echo $question->getQuestion(); ?></h2>
         <h4 style="color: blue">Choices</h4>
-        <form method="POST" action="quiz.php">
+        
         <input type="hidden" name="number" value="<?php echo $question->getNumber();?>" />
-
 
         <?php foreach ($question->getChoices() as $choice): ?>
 
-
-        <input
-            type="radio"
-            name="answer"
-            value="<?php echo $choice->letter; ?>" />
+            <input
+                type="radio"
+                name="answer<?php echo $question->getNumber(); ?>"
+                value="<?php echo $choice->letter; ?>" />
             <?php echo $choice->letter; ?>)
-        <?php echo $choice->label; ?><br />
-
+            <?php echo $choice->label; ?><br />
 
         <?php endforeach; ?>
-<?php endfor; ?>
-
-
-<input type="submit" value="Next">
+        
+    <?php endfor; ?>
+    <input type="submit" value="Submit Answer" />
 </form>
-
-
-</body>
-</html>
-
 
 <!-- DEBUG MODE -->
 <pre>
@@ -118,3 +101,6 @@ for ($number = 1; $number <= 10; $number++): ?>
 var_dump($_SESSION);
 ?>
 </pre>
+
+</body>
+</html>
